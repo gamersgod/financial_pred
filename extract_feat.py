c3a1e565bf84becd2d76b12e1cc77af0a51bce4d
huajie chen <cmathking@gmail.com>
2018-10-27 14:00:28 +0800

    update

--- extract_feat.py
+++ extract_feat.py
@@ -2,22 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-def get_eluer_dis(x, y):
-    return np.linalg.norm(x - y)
-
-
-def get_cosin_dis(x, y):
-    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
-
-
 def get_next_day(curr_day):
     idx = all_date.index(curr_day)
     return all_date[idx+1]
 
 
 def get_feat(start, w_size, have_label=True):
-    cols = [e for e in range(start, start+w_size)]
-    fund_names = fund_data["fund_name"].tolist()
+    end = start + w_size
+    cols = [e for e in range(start, end)]
     fund_tmp = fund_data.iloc[:, cols]
     bench_tmp = bench_data.iloc[:, cols]
     index_tmp = index_data.iloc[:, cols]
@@ -43,11 +35,11 @@ def get_feat(start, w_size, have_label=True):
     t["fund_mean_30"] = fund_tmp.iloc[:, -30:].mean(axis=1)
     t["fund_std_30"] = fund_tmp.iloc[:, -30:].std(axis=1)
 
-    fund_diff_value = np.diff(fund_tmp.values)
-    t["fund_diff_max"] = np.max(fund_diff_value, axis=1)
-    t["fund_diff_min"] = np.min(fund_diff_value, axis=1)
-    t["fund_diff_mean"] = np.mean(fund_diff_value, axis=1)
-    t["fund_diff_std"] = np.std(fund_diff_value, axis=1)
+    fund_diff= fund_tmp.diff(axis=1).iloc[:, 1:]
+    t["fund_diff_max"] = np.max(fund_diff, axis=1)
+    t["fund_diff_min"] = np.min(fund_diff, axis=1)
+    t["fund_diff_mean"] = np.mean(fund_diff, axis=1)
+    t["fund_diff_std"] = np.std(fund_diff, axis=1)
 
     t["bench_max"] = bench_tmp.max(axis=1)
     t["bench_min"] = bench_tmp.min(axis=1)
@@ -69,207 +61,160 @@ def get_feat(start, w_size, have_label=True):
     t["bench_mean_30"] = bench_tmp.iloc[:, -30:].mean(axis=1)
     t["bench_std_30"] = bench_tmp.iloc[:, -30:].std(axis=1)
 
-    bench_diff_value = np.diff(bench_tmp.values)
-    t["benchmark_diff_max"] = np.max(bench_diff_value, axis=1)
-    t["benchmark_diff_min"] = np.min(bench_diff_value, axis=1)
-    t["benchmark_diff_mean"] = np.mean(bench_diff_value, axis=1)
-    t["benchmark_diff_std"] = np.std(bench_diff_value, axis=1)
+    bench_diff = bench_tmp.diff(axis=1).iloc[:, 1:]
+    t["benchmark_diff_max"] = np.max(bench_diff, axis=1)
+    t["benchmark_diff_min"] = np.min(bench_diff, axis=1)
+    t["benchmark_diff_mean"] = np.mean(bench_diff, axis=1)
+    t["benchmark_diff_std"] = np.std(bench_diff, axis=1)
 
-    # sub_tmp = fund_tmp.sub(bench_tmp)
-    # t["sub_max"] = sub_tmp.max(axis=1)
-    # t["sub_min"] = sub_tmp.min(axis=1)
-    # t["sub_mean"] = sub_tmp.mean(axis=1)
-    # t["sub_std"] = sub_tmp.std(axis=1)
-    #
-    # t["sub_max_7"] = sub_tmp.iloc[:, -7:].max(axis=1)
-    # t["sub_min_7"] = sub_tmp.iloc[:, -7:].min(axis=1)
-    # t["sub_mean_7"] = sub_tmp.iloc[:, -7:].mean(axis=1)
-    # t["sub_std_7"] = sub_tmp.iloc[:, -7:].std(axis=1)
-    #
-    # t["sub_max_15"] = sub_tmp.iloc[:, -15:].max(axis=1)
-    # t["sub_min_15"] = sub_tmp.iloc[:, -15:].min(axis=1)
-    # t["sub_mean_15"] = sub_tmp.iloc[:, -15:].mean(axis=1)
-    # t["sub_std_15"] = sub_tmp.iloc[:, -15:].std(axis=1)
-    #
-    # t["sub_max_30"] = sub_tmp.iloc[:, -30:].max(axis=1)
-    # t["sub_min_30"] = sub_tmp.iloc[:, -30:].min(axis=1)
-    # t["sub_mean_30"] = sub_tmp.iloc[:, -30:].mean(axis=1)
-    # t["sub_std_30"] = sub_tmp.iloc[:, -30:].std(axis=1)
-    #
-    # div0_tmp = fund_tmp.iloc.div(bench_tmp)
-    # t["div0_max"] = div0_tmp.max(axis=1)
-    # t["div0_min"] = div0_tmp.min(axis=1)
-    # t["div0_mean"] = div0_tmp.mean(axis=1)
-    # t["div0_std"] = div0_tmp.std(axis=1)
-    #
-    # div1_tmp = bench_tmp.div(fund_tmp)
-    # t["div1_max"] = div1_tmp.max(axis=1)
-    # t["div1_min"] = div1_tmp.min(axis=1)
-    # t["div1_mean"] = div1_tmp.mean(axis=1)
-    # t["div1_std"] = div1_tmp.std(axis=1)
-    #
-    # t["fb_max_diff"] = t["fund_max"] - t["benchmark_max"]
-    # t["fb_min_diff"] = t["fund_min"] - t["benchmark_min"]
-    # t["fb_mean_diff"] = t["fund_mean"] - t["benchmark_mean"]
-    # t["fb_std_diff"] = t["fund_std"] - t["benchmark_std"]
-    #
-    # t["fb_diff_max_diff"] = t["fund_diff_max"] - t["benchmark_diff_max"]
-    # t["fb_diff_min_diff"] = t["fund_diff_min"] - t["benchmark_diff_min"]
-    # t["fb_diff_mean_diff"] = t["fund_diff_mean"] - t["benchmark_diff_mean"]
-    # t["fb_diff_std_diff"] = t["fund_diff_std"] - t["benchmark_diff_std"]
-    #
-    # t["fb_max_rate"] = t["fund_max"] / t["benchmark_max"]
-    # t["fb_min_rate"] = t["fund_min"] / t["benchmark_min"]
-    # t["fb_mean_rate"] = t["fund_mean"] / t["benchmark_mean"]
-    # t["fb_std_rate"] = t["fund_std"] / t["benchmark_std"]
-    #
-    # t["fb_diff_max_rate"] = t["fund_diff_max"] / t["benchmark_diff_max"]
-    # t["fb_diff_min_rate"] = t["fund_diff_min"] / t["benchmark_diff_min"]
-    # t["fb_diff_mean_rate"] = t["fund_diff_mean"] / t["benchmark_diff_mean"]
-    # t["fb_diff_std_rate"] = t["fund_diff_std"] / t["benchmark_diff_std"]
+    t["value"] = 1
+    d = pd.merge(t, t, how="inner", on="value")
+    d["fund_name_x_"] = d["fund_name_x"].apply(lambda i: int(i.split()[-1]))
+    d["fund_name_y_"] = d["fund_name_y"].apply(lambda i: int(i.split()[-1]))
+    d = d[d["fund_name_y_"] > d["fund_name_x_"]]
 
-    columns = t.columns.tolist() + [i+"_x" for i in t.columns[1:].tolist()] + \
-                                                                               ["fund_eluer",
-                                                                               "fund_cosin",
-                                                                               "bench_eluer",
-                                                                               "bench_cosin",
-                                                                               "fund_diff_eluer",
-                                                                               "fund_diff_cosin",
-                                                                               "bench_diff_eluer",
-                                                                               "bench_diff_cosin",
-                                                                               "fund_eluer_7",
-                                                                               "fund_eluer_15",
-                                                                               "fund_eluer_30",
-                                                                               "fund_cosin_7",
-                                                                               "fund_cosin_15",
-                                                                               "fund_cosin_30",
-                                                                               "bench_eluer_7",
-                                                                               "bench_eluer_15",
-                                                                               "bench_eluer_30",
-                                                                               "bench_cosin_7",
-                                                                               "bench_cosin_15",
-                                                                               "bench_cosin_30",
-                                                                               "fund_diff_eluer_7",
-                                                                               "fund_diff_eluer_15",
-                                                                               "fund_diff_eluer_30",
-                                                                               "fund_diff_cosin_7",
-                                                                               "fund_diff_cosin_15",
-                                                                               "fund_diff_cosin_30",
-                                                                               "bench_diff_eluer_7",
-                                                                               "bench_diff_eluer_15",
-                                                                               "bench_diff_eluer_30",
-                                                                               "bench_diff_cosin_7",
-                                                                               "bench_diff_cosin_15",
-                                                                               "bench_diff_cosin_30"]
+    x_index = d["fund_name_x_"].values - 1
+    y_index = d["fund_name_y_"].values - 1
 
-    t1 = pd.DataFrame(columns=columns)
-    index = 0
-    for i in range(len(fund_names)):
-        for j in range(i + 1, len(fund_names)):
-            d = [fund_names[i] + "-" + fund_names[j]] + t.iloc[i, 1:].append(t.iloc[j, 1:], ignore_index=True).tolist()
+    decare_iindex = [x_index, y_index]
 
-            fund_x, fund_y = fund_tmp.iloc[i, :].values, fund_tmp.iloc[j, :].values
-            fund_eluer = get_eluer_dis(fund_x, fund_y)
-            fund_cosin = get_cosin_dis(fund_x, fund_y)
+    d["fund_name"] = d["fund_name_x"] + "-" + d["fund_name_y"]
+    d1 = d.drop(["fund_name_x", "fund_name_y", "fund_name_x_", "fund_name_y_", "value"], axis=1)
 
-            fund_x_7, fund_y_7 = fund_tmp.iloc[i, -7:].values, fund_tmp.iloc[j, -7:].values
-            fund_eluer_7 = get_eluer_dis(fund_x_7, fund_y_7)
-            fund_cosin_7 = get_cosin_dis(fund_x_7, fund_y_7)
+    fund_corr = fund_tmp.T.corr().values[decare_iindex]
+    d1["fund_cosin"] = fund_corr
 
-            fund_x_15, fund_y_15 = fund_tmp.iloc[i, -15:].values, fund_tmp.iloc[j, -15:].values
-            fund_eluer_15 = get_eluer_dis(fund_x_15, fund_y_15)
-            fund_cosin_15 = get_cosin_dis(fund_x_15, fund_y_15)
+    fund_corr_7 = fund_tmp.iloc[:, -7:].T.corr().values[decare_iindex]
+    d1["fund_cosin_7"] = fund_corr_7
 
-            fund_x_30, fund_y_30 = fund_tmp.iloc[i, -30:].values, fund_tmp.iloc[j, -30:].values
-            fund_eluer_30 = get_eluer_dis(fund_x_30, fund_y_30)
-            fund_cosin_30 = get_cosin_dis(fund_x_30, fund_y_30)
+    fund_corr_15 = fund_tmp.iloc[:, -15:].T.corr().values[decare_iindex]
+    d1["fund_cosin_15"] = fund_corr_15
 
-            bench_x, bench_y = bench_tmp.iloc[i, :].values, bench_tmp.iloc[j, :].values
-            bench_eluer = get_eluer_dis(bench_x, bench_y)
-            bench_cosin = get_cosin_dis(bench_x, bench_y)
+    fund_corr_30 = fund_tmp.iloc[:, -30:].T.corr().values[decare_iindex]
+    d1["fund_cosin_30"] = fund_corr_30
 
-            bench_x_7, bench_y_7 = bench_tmp.iloc[i, -7:].values, bench_tmp.iloc[j, -7:].values
-            bench_eluer_7 = get_eluer_dis(bench_x_7, bench_y_7)
-            bench_cosin_7 = get_cosin_dis(bench_x_7, bench_y_7)
+    bench_corr = bench_tmp.T.corr().values[decare_iindex]
+    d1["bench_cosin"] = bench_corr
 
-            bench_x_15, bench_y_15 = bench_tmp.iloc[i, -15:].values, bench_tmp.iloc[j, -15:].values
-            bench_eluer_15 = get_eluer_dis(bench_x_15, bench_y_15)
-            bench_cosin_15 = get_cosin_dis(bench_x_15, bench_y_15)
+    bench_corr_7 = bench_tmp.iloc[:, -7:].T.corr().values[decare_iindex]
+    d1["bench_cosin_7"] = bench_corr_7
 
-            bench_x_30, bench_y_30 = bench_tmp.iloc[i, -30:].values, bench_tmp.iloc[j, -30:].values
-            bench_eluer_30 = get_eluer_dis(bench_x_30, bench_y_30)
-            bench_cosin_30 = get_cosin_dis(bench_x_30, bench_y_30)
+    bench_corr_15 = bench_tmp.iloc[:, -15:].T.corr().values[decare_iindex]
+    d1["bench_cosin_15"] = bench_corr_15
 
-            fund_diff_x, fund_diff_y = fund_diff_value[i, :], fund_diff_value[j, :]
-            fund_diff_eluer = get_eluer_dis(fund_diff_x, fund_diff_y)
-            fund_diff_cosin = get_cosin_dis(fund_diff_x, fund_diff_y)
+    bench_corr_30 = bench_tmp.iloc[:, -30:].T.corr().values[decare_iindex]
+    d1["bench_cosin_30"] = bench_corr_30
 
-            fund_diff_x_7, fund_diff_y_7 = fund_diff_value[i, -7:], fund_diff_value[j, -7:]
-            fund_diff_eluer_7 = get_eluer_dis(fund_diff_x_7, fund_diff_y_7)
-            fund_diff_cosin_7 = get_cosin_dis(fund_diff_x_7, fund_diff_y_7)
+    fund_diff_corr = fund_diff.T.corr().values[decare_iindex]
+    d1["fund_diff_cosin"] = fund_diff_corr
 
-            fund_diff_x_15, fund_diff_y_15 = fund_diff_value[i, -15:], fund_diff_value[j, -15:]
-            fund_diff_eluer_15 = get_eluer_dis(fund_diff_x_15, fund_diff_y_15)
-            fund_diff_cosin_15 = get_cosin_dis(fund_diff_x_15, fund_diff_y_15)
+    fund_diff_corr_7 = fund_diff.iloc[:, -7:].T.corr().values[decare_iindex]
+    d1["fund_diff_cosin_7"] = fund_diff_corr_7
 
-            fund_diff_x_30, fund_diff_y_30 = fund_diff_value[i, -30:], fund_diff_value[j, -30:]
-            fund_diff_eluer_30 = get_eluer_dis(fund_diff_x_30, fund_diff_y_30)
-            fund_diff_cosin_30 = get_cosin_dis(fund_diff_x_30, fund_diff_y_30)
+    fund_diff_corr_15 = fund_diff.iloc[:, -15:].T.corr().values[decare_iindex]
+    d1["fund_diff_cosin_15"] = fund_diff_corr_15
 
-            bench_diff_x, bench_diff_y = bench_diff_value[i, :], bench_diff_value[j, :]
-            bench_diff_eluer = get_eluer_dis(bench_diff_x, bench_diff_y)
-            bench_diff_cosin = get_cosin_dis(bench_diff_x, bench_diff_y)
+    fund_diff_corr_30 = fund_diff.iloc[:, -30:].T.corr().values[decare_iindex]
+    d1["fund_diff_cosin_30"] = fund_diff_corr_30
 
-            bench_diff_x_7, bench_diff_y_7 = bench_diff_value[i, -7:], bench_diff_value[j, -7:]
-            bench_diff_eluer_7 = get_eluer_dis(bench_diff_x_7, bench_diff_y_7)
-            bench_diff_cosin_7 = get_cosin_dis(bench_diff_x_7, bench_diff_y_7)
+    bench_diff_corr = bench_diff.T.corr().values[decare_iindex]
+    d1["bench_diff_cosin"] = bench_diff_corr
 
-            bench_diff_x_15, bench_diff_y_15 = bench_diff_value[i, -15:], bench_diff_value[j, -15:]
-            bench_diff_eluer_15 = get_eluer_dis(bench_diff_x_15, bench_diff_y_15)
-            bench_diff_cosin_15 = get_cosin_dis(bench_diff_x_15, bench_diff_y_15)
+    bench_diff_corr_7 = bench_diff.iloc[:, -7:].T.corr().values[decare_iindex]
+    d1["bench_diff_cosin_7"] = bench_diff_corr_7
 
-            bench_diff_x_30, bench_diff_y_30 = bench_diff_value[i, -30:], bench_diff_value[j, -30:]
-            bench_diff_eluer_30 = get_eluer_dis(bench_diff_x_30, bench_diff_y_30)
-            bench_diff_cosin_30 = get_cosin_dis(bench_diff_x_30, bench_diff_y_30)
+    bench_diff_corr_15 = bench_diff.iloc[:, -15:].T.corr().values[decare_iindex]
+    d1["bench_diff_cosin_15"] = bench_diff_corr_15
 
-            t1.loc[index] = d + [fund_eluer, fund_cosin, bench_eluer, bench_cosin, fund_diff_eluer,
-                                 fund_diff_cosin, bench_diff_eluer, bench_diff_cosin, fund_eluer_7,
-                                 fund_eluer_15, fund_eluer_30, fund_cosin_7, fund_cosin_15, fund_cosin_30,
-                                 bench_eluer_7, bench_eluer_15, bench_eluer_30, bench_cosin_7,
-                                 bench_cosin_15, bench_cosin_30, fund_diff_eluer_7, fund_diff_eluer_15,
-                                 fund_diff_eluer_30, fund_diff_cosin_7, fund_diff_cosin_15,
-                                 fund_diff_cosin_30, bench_diff_eluer_7, bench_diff_eluer_15,
-                                 bench_diff_eluer_30, bench_diff_cosin_7, bench_diff_cosin_15,
-                                 bench_diff_cosin_30]
-            index += 1
+    bench_diff_corr_30 = bench_diff.iloc[:, -30:].T.corr().values[decare_iindex]
+    d1["bench_diff_cosin_30"] = bench_diff_corr_30
 
+    fund_corr = fund_tmp.T.corr("spearman").values[decare_iindex]
+    d1["fund_spearman"] = fund_corr
+
+    fund_corr_7 = fund_tmp.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
+    d1["fund_spearman_7"] = fund_corr_7
+
+    fund_corr_15 = fund_tmp.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
+    d1["fund_spearman_15"] = fund_corr_15
+
+    fund_corr_30 = fund_tmp.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
+    d1["fund_spearman_30"] = fund_corr_30
+
+    bench_corr = bench_tmp.T.corr("spearman").values[decare_iindex]
+    d1["bench_spearman"] = bench_corr
+
+    bench_corr_7 = bench_tmp.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
+    d1["bench_spearman_7"] = bench_corr_7
+
+    bench_corr_15 = bench_tmp.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
+    d1["bench_spearman_15"] = bench_corr_15
+
+    bench_corr_30 = bench_tmp.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
+    d1["bench_spearman_30"] = bench_corr_30
+
+    fund_diff_corr = fund_diff.T.corr("spearman").values[decare_iindex]
+    d1["fund_diff_spearman"] = fund_diff_corr
+
+    fund_diff_corr_7 = fund_diff.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
+    d1["fund_diff_spearman_7"] = fund_diff_corr_7
+
+    fund_diff_corr_15 = fund_diff.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
+    d1["fund_diff_spearman_15"] = fund_diff_corr_15
+
+    fund_diff_corr_30 = fund_diff.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
+    d1["fund_diff_spearman_30"] = fund_diff_corr_30
+
+    bench_diff_corr = bench_diff.T.corr("spearman").values[decare_iindex]
+    d1["bench_diff_spearman"] = bench_diff_corr
+
+    bench_diff_corr_7 = bench_diff.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
+    d1["bench_diff_spearman_7"] = bench_diff_corr_7
+
+    bench_diff_corr_15 = bench_diff.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
+    d1["bench_diff_spearman_15"] = bench_diff_corr_15
+
+    bench_diff_corr_30 = bench_diff.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
+    d1["bench_diff_spearman_30"] = bench_diff_corr_30
+
+    # 相关性特征
+    corr_value = corr_data.iloc[:, start-15:start]
+    d1["corr_max_15"] = corr_value.max(axis=1)
+    d1["corr_min_15"] = corr_value.min(axis=1)
+    d1["corr_mean_15"] = corr_value.mean(axis=1)
+    d1["corr_std_15"] = corr_value.std(axis=1)
+
+    corr_value = corr_data.iloc[:, start-7:start]
+    d1["corr_max_7"] = corr_value.max(axis=1)
+    d1["corr_min_7"] = corr_value.min(axis=1)
+    d1["corr_mean_7"] = corr_value.mean(axis=1)
+    d1["corr_std_7"] = corr_value.std(axis=1)
 
     if have_label:
         next_day = get_next_day(index_tmp.columns[-1])
-        t1["label"] = corr_data[next_day]
+        d1["label"] = corr_data[next_day]
         print(next_day)
 
-    return t1
+    return d1
 
 
 if __name__ == '__main__':
-    fund_data = pd.read_csv("new_dataset/fund_data.csv")
-    bench_data = pd.read_csv("new_dataset/bench_data.csv")
-    index_data = pd.read_csv("new_dataset/index_data.csv", encoding="gbk")
-    corr_data = pd.read_csv("new_dataset/corr_data.csv")
+    fund_data = pd.read_csv("dataset/fund_data.csv")
+    bench_data = pd.read_csv("dataset/bench_data.csv")
+    index_data = pd.read_csv("dataset/index_data.csv", encoding="gbk")
+    corr_data = pd.read_csv("dataset/corr_data.csv")
     all_date = corr_data.columns[1:].tolist()
 
-    # TODO: 两个基金之前的相关性特征，只能取前多少天的，但是预测的时候有60多天corr数据缺失，怎么办
     w_size = 60 # 窗口大小
-    delta = 30 # 步长
-    data_list = []
-    for i in range(300, 481, delta):
-        data = get_feat(i, w_size).iloc[:, 1:]
-        data_list.append(data)
+    # delta = 30 # 步长
+    # data_list = []
+    # for i in range(1, 481, delta):
+    #     data = get_feat(i, w_size).iloc[:, 1:]
+    #     data_list.append(data)
 
-    train_data = pd.concat(data_list, axis=0)
-    print(train_data.shape)
-    train_data.to_csv("new_data/train_data300_481_30.csv", index=None)
+    # train_data = pd.concat(data_list, axis=0)
+    # print(train_data.shape)
+    # train_data.to_csv("new_data/train_data1_481_30.csv", index=None)
 
-    # test_data = get_feat(541, 60, have_label=False)
+    test_data = get_feat(541, w_size, have_label=False)
     # test_data.to_csv("data/test_data.csv", index=None)
