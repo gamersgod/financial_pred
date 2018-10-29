796f3e0e07235fe3b57d430477d3777453b4a716
gamersover <cmathking@gmail.com>
2018-10-29 14:50:20 +0800

    update

--- extract_feat.py
+++ extract_feat.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial.distance import cdist
 
 
 def get_next_day(curr_day):
@@ -14,6 +15,13 @@ def get_feat(start, w_size, have_label=True):
     fund_tmp = fund_data.iloc[:, cols]
     bench_tmp = bench_data.iloc[:, cols]
     index_tmp = index_data.iloc[:, cols]
+    fund_diff= fund_tmp.diff(axis=1).iloc[:, 1:]
+    bench_diff = bench_tmp.diff(axis=1).iloc[:, 1:]
+
+    fund_tmp_7 = fund_tmp.iloc[:, -7:]
+    bench_tmp_7 = bench_tmp.iloc[:, -7:]
+    fund_diff_7 = fund_diff.iloc[:, -7:]
+    bench_diff_7 = bench_diff.iloc[:, -7:]
 
     t = fund_data[["fund_name"]]
     t["fund_max"] = fund_tmp.max(axis=1)
@@ -21,22 +29,11 @@ def get_feat(start, w_size, have_label=True):
     t["fund_mean"] = fund_tmp.mean(axis=1)
     t["fund_std"] = fund_tmp.std(axis=1)
 
-    t["fund_max_7"] = fund_tmp.iloc[:, -7:].max(axis=1)
-    t["fund_min_7"] = fund_tmp.iloc[:, -7:].min(axis=1)
-    t["fund_mean_7"] = fund_tmp.iloc[:, -7:].mean(axis=1)
-    t["fund_std_7"] = fund_tmp.iloc[:, -7:].std(axis=1)
+    t["fund_max_7"] = fund_tmp_7.max(axis=1)
+    t["fund_min_7"] = fund_tmp_7.min(axis=1)
+    t["fund_mean_7"] = fund_tmp_7.mean(axis=1)
+    t["fund_std_7"] = fund_tmp_7.std(axis=1)
 
-    t["fund_max_15"] = fund_tmp.iloc[:, -15:].max(axis=1)
-    t["fund_min_15"] = fund_tmp.iloc[:, -15:].min(axis=1)
-    t["fund_mean_15"] = fund_tmp.iloc[:, -15:].mean(axis=1)
-    t["fund_std_15"] = fund_tmp.iloc[:, -15:].std(axis=1)
-
-    t["fund_max_30"] = fund_tmp.iloc[:, -30:].max(axis=1)
-    t["fund_min_30"] = fund_tmp.iloc[:, -30:].min(axis=1)
-    t["fund_mean_30"] = fund_tmp.iloc[:, -30:].mean(axis=1)
-    t["fund_std_30"] = fund_tmp.iloc[:, -30:].std(axis=1)
-
-    fund_diff= fund_tmp.diff(axis=1).iloc[:, 1:]
     t["fund_diff_max"] = np.max(fund_diff, axis=1)
     t["fund_diff_min"] = np.min(fund_diff, axis=1)
     t["fund_diff_mean"] = np.mean(fund_diff, axis=1)
@@ -47,26 +44,15 @@ def get_feat(start, w_size, have_label=True):
     t["bench_mean"] = bench_tmp.mean(axis=1)
     t["bench_std"] = bench_tmp.std(axis=1)
 
-    t["bench_max_7"] = bench_tmp.iloc[:, -7:].max(axis=1)
-    t["bench_min_7"] = bench_tmp.iloc[:, -7:].min(axis=1)
-    t["bench_mean_7"] = bench_tmp.iloc[:, -7:].mean(axis=1)
-    t["bench_std_7"] = bench_tmp.iloc[:, -7:].std(axis=1)
+    t["bench_max_7"] = bench_tmp_7.max(axis=1)
+    t["bench_min_7"] = bench_tmp_7.min(axis=1)
+    t["bench_mean_7"] = bench_tmp_7.mean(axis=1)
+    t["bench_std_7"] = bench_tmp_7.std(axis=1)
 
-    t["bench_max_15"] = bench_tmp.iloc[:, -15:].max(axis=1)
-    t["bench_min_15"] = bench_tmp.iloc[:, -15:].min(axis=1)
-    t["bench_mean_15"] = bench_tmp.iloc[:, -15:].mean(axis=1)
-    t["bench_std_15"] = bench_tmp.iloc[:, -15:].std(axis=1)
-
-    t["bench_max_30"] = bench_tmp.iloc[:, -30:].max(axis=1)
-    t["bench_min_30"] = bench_tmp.iloc[:, -30:].min(axis=1)
-    t["bench_mean_30"] = bench_tmp.iloc[:, -30:].mean(axis=1)
-    t["bench_std_30"] = bench_tmp.iloc[:, -30:].std(axis=1)
-
-    bench_diff = bench_tmp.diff(axis=1).iloc[:, 1:]
-    t["benchmark_diff_max"] = np.max(bench_diff, axis=1)
-    t["benchmark_diff_min"] = np.min(bench_diff, axis=1)
-    t["benchmark_diff_mean"] = np.mean(bench_diff, axis=1)
-    t["benchmark_diff_std"] = np.std(bench_diff, axis=1)
+    t["bench_diff_max"] = np.max(bench_diff, axis=1)
+    t["bench_diff_min"] = np.min(bench_diff, axis=1)
+    t["bench_diff_mean"] = np.mean(bench_diff, axis=1)
+    t["bench_diff_std"] = np.std(bench_diff, axis=1)
 
     t["value"] = 1
     d = pd.merge(t, t, how="inner", on="value")
@@ -81,162 +67,86 @@ def get_feat(start, w_size, have_label=True):
     d["fund_name"] = d["fund_name_x"] + "-" + d["fund_name_y"]
     d1 = d.drop(["fund_name_x", "fund_name_y", "fund_name_x_", "fund_name_y_", "value"], axis=1)
 
-    fund_corr = fund_tmp.T.corr().values[decare_iindex]
-    d1["fund_pearson"] = fund_corr
+    def get_pandas_corr(corr_type):
+        fund_corr = fund_tmp.T.corr(corr_type).values[decare_iindex]
+        d1["fund_" + corr_type] = fund_corr
 
-    fund_corr_7 = fund_tmp.iloc[:, -7:].T.corr().values[decare_iindex]
-    d1["fund_pearson_7"] = fund_corr_7
+        fund_corr_7 = fund_tmp_7.T.corr(corr_type).values[decare_iindex]
+        d1["fund_" + corr_type + "_7"] = fund_corr_7
 
-    fund_corr_15 = fund_tmp.iloc[:, -15:].T.corr().values[decare_iindex]
-    d1["fund_pearson_15"] = fund_corr_15
+        bench_corr = bench_tmp.T.corr(corr_type).values[decare_iindex]
+        d1["bench_" + corr_type] = bench_corr
 
-    fund_corr_30 = fund_tmp.iloc[:, -30:].T.corr().values[decare_iindex]
-    d1["fund_pearson_30"] = fund_corr_30
+        bench_corr_7 = bench_tmp_7.T.corr(corr_type).values[decare_iindex]
+        d1["bench_" + corr_type + "_7"] = bench_corr_7
 
-    bench_corr = bench_tmp.T.corr().values[decare_iindex]
-    d1["bench_pearson"] = bench_corr
+        fund_diff_corr = fund_diff.T.corr(corr_type).values[decare_iindex]
+        d1["fund_diff_" + corr_type] = fund_diff_corr
 
-    bench_corr_7 = bench_tmp.iloc[:, -7:].T.corr().values[decare_iindex]
-    d1["bench_pearson_7"] = bench_corr_7
+        fund_diff_corr_7 = fund_diff_7.T.corr(corr_type).values[decare_iindex]
+        d1["fund_diff_" + corr_type + "_7"] = fund_diff_corr_7
 
-    bench_corr_15 = bench_tmp.iloc[:, -15:].T.corr().values[decare_iindex]
-    d1["bench_pearson_15"] = bench_corr_15
+        bench_diff_corr = bench_diff.T.corr(corr_type).values[decare_iindex]
+        d1["bench_diff_" + corr_type] = bench_diff_corr
 
-    bench_corr_30 = bench_tmp.iloc[:, -30:].T.corr().values[decare_iindex]
-    d1["bench_pearson_30"] = bench_corr_30
+        bench_diff_corr_7 = bench_diff_7.T.corr(corr_type).values[decare_iindex]
+        d1["bench_diff_" + corr_type + "_7"] = bench_diff_corr_7
 
-    fund_diff_corr = fund_diff.T.corr().values[decare_iindex]
-    d1["fund_diff_pearson"] = fund_diff_corr
+    def get_sklearn_dist(metric):
+        fund_corr = cdist(fund_tmp, fund_tmp, metric=metric)[decare_iindex]
+        d1["fund_" + metric] = fund_corr
 
-    fund_diff_corr_7 = fund_diff.iloc[:, -7:].T.corr().values[decare_iindex]
-    d1["fund_diff_pearson_7"] = fund_diff_corr_7
+        fund_corr_7 = cdist(fund_tmp_7,fund_tmp_7, metric=metric)[decare_iindex]
+        d1["fund_" + metric + "_7"] = fund_corr_7
 
-    fund_diff_corr_15 = fund_diff.iloc[:, -15:].T.corr().values[decare_iindex]
-    d1["fund_diff_pearson_15"] = fund_diff_corr_15
+        bench_corr = cdist(bench_tmp, bench_tmp, metric=metric)[decare_iindex]
+        d1["bench_" + metric] = bench_corr
 
-    fund_diff_corr_30 = fund_diff.iloc[:, -30:].T.corr().values[decare_iindex]
-    d1["fund_diff_pearson_30"] = fund_diff_corr_30
+        bench_corr_7 = cdist(bench_tmp_7, bench_tmp_7, metric=metric)[decare_iindex]
+        d1["bench_" + metric + "_7"] = bench_corr_7
 
-    bench_diff_corr = bench_diff.T.corr().values[decare_iindex]
-    d1["bench_diff_pearson"] = bench_diff_corr
+        fund_diff_corr = cdist(fund_diff, fund_diff, metric=metric)[decare_iindex]
+        d1["fund_diff_" + metric] = fund_diff_corr
 
-    bench_diff_corr_7 = bench_diff.iloc[:, -7:].T.corr().values[decare_iindex]
-    d1["bench_diff_pearson_7"] = bench_diff_corr_7
+        fund_diff_corr_7 = cdist(fund_diff_7, fund_diff_7, metric=metric)[decare_iindex]
+        d1["fund_diff_" + metric + "_7"] = fund_diff_corr_7
 
-    bench_diff_corr_15 = bench_diff.iloc[:, -15:].T.corr().values[decare_iindex]
-    d1["bench_diff_pearson_15"] = bench_diff_corr_15
+        bench_diff_corr = cdist(bench_diff, bench_diff, metric=metric)[decare_iindex]
+        d1["bench_diff_" + metric] = bench_diff_corr
 
-    bench_diff_corr_30 = bench_diff.iloc[:, -30:].T.corr().values[decare_iindex]
-    d1["bench_diff_pearson_30"] = bench_diff_corr_30
+        bench_diff_corr_7 = cdist(bench_diff_7, bench_diff_7, metric=metric)[decare_iindex]
+        d1["bench_diff_" + metric + "_7"] = bench_diff_corr_7
 
-    fund_corr = fund_tmp.T.corr("spearman").values[decare_iindex]
-    d1["fund_spearman"] = fund_corr
+    get_pandas_corr("pearson")
+    get_pandas_corr("spearman")
+    # get_pandas_corr("kendall")
+    get_sklearn_dist("cosine")
+    # get_sklearn_dist("euclidean")
+    # get_sklearn_dist("seuclidean")
+    # get_sklearn_dist("mahalanobis")
+    # get_sklearn_dist("braycurtis")
+    # get_sklearn_dist("canberra")
+    # get_sklearn_dist("chebyshev")
 
-    fund_corr_7 = fund_tmp.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
-    d1["fund_spearman_7"] = fund_corr_7
-
-    fund_corr_15 = fund_tmp.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
-    d1["fund_spearman_15"] = fund_corr_15
-
-    fund_corr_30 = fund_tmp.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
-    d1["fund_spearman_30"] = fund_corr_30
-
-    bench_corr = bench_tmp.T.corr("spearman").values[decare_iindex]
-    d1["bench_spearman"] = bench_corr
-
-    bench_corr_7 = bench_tmp.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
-    d1["bench_spearman_7"] = bench_corr_7
-
-    bench_corr_15 = bench_tmp.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
-    d1["bench_spearman_15"] = bench_corr_15
-
-    bench_corr_30 = bench_tmp.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
-    d1["bench_spearman_30"] = bench_corr_30
-
-    fund_diff_corr = fund_diff.T.corr("spearman").values[decare_iindex]
-    d1["fund_diff_spearman"] = fund_diff_corr
-
-    fund_diff_corr_7 = fund_diff.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
-    d1["fund_diff_spearman_7"] = fund_diff_corr_7
-
-    fund_diff_corr_15 = fund_diff.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
-    d1["fund_diff_spearman_15"] = fund_diff_corr_15
-
-    fund_diff_corr_30 = fund_diff.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
-    d1["fund_diff_spearman_30"] = fund_diff_corr_30
-
-    bench_diff_corr = bench_diff.T.corr("spearman").values[decare_iindex]
-    d1["bench_diff_spearman"] = bench_diff_corr
-
-    bench_diff_corr_7 = bench_diff.iloc[:, -7:].T.corr("spearman").values[decare_iindex]
-    d1["bench_diff_spearman_7"] = bench_diff_corr_7
-
-    bench_diff_corr_15 = bench_diff.iloc[:, -15:].T.corr("spearman").values[decare_iindex]
-    d1["bench_diff_spearman_15"] = bench_diff_corr_15
-
-    bench_diff_corr_30 = bench_diff.iloc[:, -30:].T.corr("spearman").values[decare_iindex]
-    d1["bench_diff_spearman_30"] = bench_diff_corr_30
-
-    fund_corr = cosine_similarity(fund_tmp)[decare_iindex]
-    d1["fund_cosin"] = fund_corr
-
-    fund_corr_7 = cosine_similarity(fund_tmp.iloc[:, -7:])[decare_iindex]
-    d1["fund_cosin_7"] = fund_corr_7
-
-    fund_corr_15 = cosine_similarity(fund_tmp.iloc[:, -15:])[decare_iindex]
-    d1["fund_cosin_15"] = fund_corr_15
-
-    fund_corr_30 = cosine_similarity(fund_tmp.iloc[:, -30:])[decare_iindex]
-    d1["fund_cosin_30"] = fund_corr_30
-
-    bench_corr = cosine_similarity(bench_tmp)[decare_iindex]
-    d1["bench_cosin"] = bench_corr
-
-    bench_corr_7 = cosine_similarity(bench_tmp.iloc[:, -7:])[decare_iindex]
-    d1["bench_cosin_7"] = bench_corr_7
-
-    bench_corr_15 = cosine_similarity(bench_tmp.iloc[:, -15:])[decare_iindex]
-    d1["bench_cosin_15"] = bench_corr_15
-
-    bench_corr_30 = cosine_similarity(bench_tmp.iloc[:, -30:])[decare_iindex]
-    d1["bench_cosin_30"] = bench_corr_30
-
-    fund_diff_corr = cosine_similarity(fund_diff)[decare_iindex]
-    d1["fund_diff_cosin"] = fund_diff_corr
-
-    fund_diff_corr_7 = cosine_similarity(fund_diff.iloc[:, -7:])[decare_iindex]
-    d1["fund_diff_cosin_7"] = fund_diff_corr_7
-
-    fund_diff_corr_15 = cosine_similarity(fund_diff.iloc[:, -15:])[decare_iindex]
-    d1["fund_diff_cosin_15"] = fund_diff_corr_15
-
-    fund_diff_corr_30 = cosine_similarity(fund_diff.iloc[:, -30:])[decare_iindex]
-    d1["fund_diff_cosin_30"] = fund_diff_corr_30
-
-    bench_diff_corr = cosine_similarity(bench_diff)[decare_iindex]
-    d1["bench_diff_cosin"] = bench_diff_corr
-
-    bench_diff_corr_7 = cosine_similarity(bench_diff.iloc[:, -7:])[decare_iindex]
-    d1["bench_diff_cosin_7"] = bench_diff_corr_7
-
-    bench_diff_corr_15 = cosine_similarity(bench_diff.iloc[:, -15:])[decare_iindex]
-    d1["bench_diff_cosin_15"] = bench_diff_corr_15
-
-    bench_diff_corr_30 = cosine_similarity(bench_diff.iloc[:, -30:])[decare_iindex]
-    d1["bench_diff_cosin_30"] = bench_diff_corr_30
 
     # 相关性特征
+    corr_value = corr_data.iloc[:, start-8:start-1]
+    d1["corr_max_7"] = corr_value.max(axis=1).values
+    d1["corr_min_7"] = corr_value.min(axis=1).values
+    d1["corr_mean_7"] = corr_value.mean(axis=1).values
+    d1["corr_std_7"] = corr_value.std(axis=1).values
+
     corr_value = corr_data.iloc[:, start-16:start-1]
     d1["corr_max_15"] = corr_value.max(axis=1).values
     d1["corr_min_15"] = corr_value.min(axis=1).values
     d1["corr_mean_15"] = corr_value.mean(axis=1).values
     d1["corr_std_15"] = corr_value.std(axis=1).values
 
-    corr_value = corr_data.iloc[:, start-8:start-1]
-    d1["corr_max_7"] = corr_value.max(axis=1).values
-    d1["corr_min_7"] = corr_value.min(axis=1).values
-    d1["corr_mean_7"] = corr_value.mean(axis=1).values
-    d1["corr_std_7"] = corr_value.std(axis=1).values
+    corr_value = corr_data.iloc[:, start-62:start-1]
+    d1["corr_max"] = corr_value.max(axis=1).values
+    d1["corr_min"] = corr_value.min(axis=1).values
+    d1["corr_mean"] = corr_value.mean(axis=1).values
+    d1["corr_std"] = corr_value.std(axis=1).values
 
     if have_label:
         next_day = get_next_day(index_tmp.columns[-1])
@@ -247,22 +157,25 @@ def get_feat(start, w_size, have_label=True):
 
 
 if __name__ == '__main__':
-    fund_data = pd.read_csv("new_dataset/fund_data.csv")
-    bench_data = pd.read_csv("new_dataset/bench_data.csv")
-    index_data = pd.read_csv("new_dataset/index_data.csv", encoding="gbk")
-    corr_data = pd.read_csv("new_dataset/corr_data.csv")
+    dataset_dir = "dataset/"
+    data_dir = "data/"
+    fund_data = pd.read_csv(dataset_dir + "fund_data.csv")
+    bench_data = pd.read_csv(dataset_dir + "bench_data.csv")
+    index_data = pd.read_csv(dataset_dir + "index_data.csv", encoding="gbk")
+    corr_data = pd.read_csv(dataset_dir + "corr_data.csv")
     all_date = corr_data.columns[1:].tolist()
 
+    # 101-480
     w_size = 60 # 窗口大小
-    delta = 30 # 步长
+    delta = 5 # 步长
     data_list = []
-    for i in range(21, 481, delta):
+    for i in range(100, 481, delta):
         data = get_feat(i, w_size)
         data_list.append(data)
 
     train_data = pd.concat(data_list, axis=0)
     print(train_data.shape)
-    train_data.to_csv("new_data/train_data21_481_30.csv", index=None)
+    train_data.to_csv(data_dir + "train_data.csv", index=None)
 
     test_data = get_feat(541, w_size, have_label=False)
-    test_data.to_csv("new_data/test_data.csv", index=None)
+    test_data.to_csv(data_dir + "test_data.csv", index=None)
--- model_train.py
+++ model_train.py
@@ -8,19 +8,23 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 
-#TODO: eluer feature not so good, may be use 1 to - or / it.
-data = pd.read_csv("new_data/train_data1_481_30.csv")
+data_dir = "data/"
+
+data = pd.read_csv(data_dir + "train_data.csv")
 data = data.dropna(axis=0)
 
-data_x = data.drop("label", axis=1).values
+data_x = data.drop(["fund_name", "label"], axis=1).values
 data_y = data["label"].values
 
 print(data_x.shape)
 
-test_data = pd.read_csv("new_data/test_data.csv")
-test_x = test_data.iloc[:, 1:].values
+test_data = pd.read_csv(data_dir + "test_data.csv")
+test_x = test_data.drop("fund_name", axis=1).values
 
 print(test_x.shape)
 
@@ -28,114 +32,46 @@ N = 5
 kf = KFold(n_splits=N, random_state=42, shuffle=True)
 
 
-def train_lgb():
-
-    params = {
-        "learning_rate": 0.6,
-        "boosting_type":"gbdt",
-        "num_leaves":120,
-        # "lambda_l2" : 100,
-        "lambda_l1" : 10,
-        "feature_fraction": 0.7,
-        "bagging_fraction": 0.7,
-        "bagging_freq": 5,
-        "metric": "rmse",
-        "verbose": -1
-    }
-
-    submits = np.zeros([test_x.shape[0], N])
-    for k, (train_idx, test_idx) in enumerate(kf.split(data_x, data_y)):
-        train_x, val_x, train_y, val_y = data_x[train_idx], data_x[test_idx], data_y[train_idx], data_y[test_idx]
-        lgb_train = lgb.Dataset(train_x, train_y)
-        lgb_eval = lgb.Dataset(val_x, val_y, reference=lgb_train)
-
-        gbm = lgb.train(params, lgb_train, num_boost_round=5000, valid_sets=[lgb_train, lgb_eval],
-                        early_stopping_rounds=50, verbose_eval=50)
-        # submits[:, k] = gbm.predict(test_x, num_iteration=gbm.best_iteration)
-
-    # test_data["label"] = np.mean(submits, axis=1)
-    # test_data[["fund_name", "label"]].to_csv("sub_lgb_kflod5.csv", index=False)
-
-
-def train_xgb():
-    params = {
-        "eta" : 0.3,
-        "max_depth": 5,
-        "subsample": 0.8,
-        "colsample_bytree": 0.8,
-        "alpha": 0,
-        "lambda": 0,
-        "eval_metric":"rmse",
-        "silent":1,
-        "seed": 888
-    }
-
-    train_x, val_x, train_y, val_y = train_test_split(data_x, data_y, test_size=0.2)
-    train_data = xgb.DMatrix(train_x, train_y)
-    val_data = xgb.DMatrix(val_x, val_y)
-    model = xgb.train(params, train_data, num_boost_round=5000, evals=[(train_data, "train"), (val_data, "val")],
-              early_stopping_rounds=50, verbose_eval=50)
-
-    fscore = model.get_fscore()
-    fname = []
-    score = []
-    for k, v in fscore.items():
-        fname.append(k)
-        score.append(v)
-    pd.DataFrame({"fname": fname, "score": score}).to_csv("feat_score.csv", index=None)
-
-
-def train_cat():
-    # train_x, val_x, train_y, val_y = train_test_split(data_x, data_y, test_size=0.3)
+def train_sklearn_kflod(model):
     submits = np.zeros([test_x.shape[0], N])
     for k, (train_idx, test_idx) in enumerate(kf.split(data_x, data_y)):
         print("current flod:", k)
         train_x, val_x, train_y, val_y = data_x[train_idx], data_x[test_idx], data_y[train_idx], data_y[test_idx]
-        model = CatBoostRegressor(iterations=30000, depth=7, learning_rate=2.0, loss_function="RMSE", task_type="GPU",
-                                  use_best_model=True)
-        model.fit(train_x, train_y, eval_set=(val_x, val_y))
-        submits[:, k] = model.predict(test_x)
-    # print("loss", mean_squared_error(model.predict(val_x), val_y))
-    test_data["label"] = np.mean(submits, axis=1)
-    test_data[["fund_name", "label"]].to_csv("sub_catboost_kfold5_v2.csv", index=False)
-
-
-def train_sklearn_lgb():
-    train_x, val_x, train_y, val_y = train_test_split(data_x, data_y, test_size=0.2)
-    print("start train")
-    model = LGBMRegressor(silent=-1)
-    model.fit(train_x, train_y)
-    print("train loss", mean_squared_error(model.predict(train_x), train_y))
-    print("valid loss", mean_squared_error(model.predict(val_x), val_y))
-    test_data["2018-03-19"] = model.predict(test_x)
-    test_data[["fund_name", "2018-03-19"]].to_csv("sub_lgb_reg_v1.csv", index=None)
-
-
-def train_sklearn_lgb_kflod():
-    submits = np.zeros([test_x.shape[0], N])
-    for k, (train_idx, test_idx) in enumerate(kf.split(data_x, data_y)):
-        print("current flod:", k)
-        train_x, val_x, train_y, val_y = data_x[train_idx], data_x[test_idx], data_y[train_idx], data_y[test_idx]
-        model = LGBMRegressor()
         model.fit(train_x, train_y)
         print("train loss", mean_squared_error(model.predict(train_x), train_y))
         print("valid loss", mean_squared_error(model.predict(val_x), val_y))
         submits[:, k] = model.predict(test_x)
 
     test_data["2018-03-19"] = np.mean(submits, axis=1)
-    test_data[["fund_name", "2018-03-19"]].to_csv("sub_lgb_reg_5fold_v1.csv", index=None)
+    test_data[["fund_name", "2018-03-19"]].to_csv("sub_reg_5fold_v3.csv", index=None)
 
 
-def train_sklearn():
+def train_sklearn(model):
     train_x, val_x, train_y, val_y = train_test_split(data_x, data_y, test_size=0.2)
     print("start train")
-    model = RandomForestRegressor(verbose=2)
     model.fit(train_x, train_y)
     print("train loss", mean_squared_error(model.predict(train_x), train_y))
     print("valid loss", mean_squared_error(model.predict(val_x), val_y))
-    # test_data["2018-03-19"] = model.predict(test_x)
-    # test_data[["fund_name", "2018-03-19"]].to_csv("sub_lgb_reg_v1.csv", index=None)
+    test_data["2018-03-19"] = model.predict(test_x)
+    test_data[["fund_name", "2018-03-19"]].to_csv("sub_reg_v3.csv", index=None)
+
+
+def train_dnn():
+    train_x, val_x, train_y, val_y = train_test_split(data_x, data_y, test_size=0.2)
+    model = Sequential()
+    model.add(Dense(600, activation="relu", input_dim=train_x.shape[1]))
+    model.add(Dense(300, activation="relu"))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(1))
+
+    model.compile(loss="mse", metrics=["mae"], optimizer="adam")
+
+    model.summary()
+    model.fit(train_x, train_y, batch_size=128, epochs=5, validation_data=(val_x, val_y))
 
 
 if __name__ == '__main__':
-    train_sklearn_lgb()
+    model = XGBRegressor()
+    # model = LGBMRegressor()
+    train_sklearn(model)
